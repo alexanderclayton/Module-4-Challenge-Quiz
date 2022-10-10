7ee29061c6fed90d3d7c2fa0057b46c79202dd2b
Alex Clayton <alexander.clayton.email@gmail.com>
2022-10-10 00:54:07 -0500

    added more javascript functions

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,11 +1,12 @@
 var box1 = document.getElementById("box1");
-var start = document.getElementById("start");
-var questionText = getElementById("question-text");
-var questionOptions = getElementById("question-options");
-var optionA = getElementById("option-a");
-var optionB = getElementById("option-b");
-var optionC = getElementById("option-c");
-var optionD = getElementById("option-d");
+var start = document.getElementById("start-button");
+var questionText = document.getElementById("question-text");
+var questionOptions = document.getElementById("question-options");
+var optionA = document.getElementById("option-a");
+var optionB = document.getElementById("option-b");
+var optionC = document.getElementById("option-c");
+var optionD = document.getElementById("option-d");
+var check = document.getElementById("check");
 
 var questions = [
     {
@@ -53,15 +54,35 @@ function displayQuestion() {
     var thisQuestion = questions[currentQuestion];
 
     questionText.innerHTML = "<p>"+ thisQuestion.questionText +"</p>";
-    optionA.innerHTML = thisQuestion.optionA;
-    optionB.innerHTML = thisQuestion.optionB;
-    optionC.innerHTML = thisQuestion.optionC;
-    optionD.innerHTML = thisQuestion.optionD;
+    optionA.innerHTML = "<button class='answer-button'>"+ thisQuestion.optionA +"</button>";
+    optionB.innerHTML = "<button class='answer-button'>"+ thisQuestion.optionB +"</button>";
+    optionC.innerHTML = "<button class='answer-button'>"+ thisQuestion.optionC +"</button>";
+    optionD.innerHTML = "<button class='answer-button'>"+ thisQuestion.optionD +"</button>";
 };
 
 start.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    box1.style.display = "none"
+    box1.style.display = "none";
     displayQuestion();
+    var answerButton = document.getElementsByClassName("answer-button");
+    for (var i = 0; i < answerButton.length; i++) {
+        answerButton[i].addEventListener("click", nextQuestion)
+    };
+}
+
+function nextQuestion() {
+    box1.style.display = "none";
+    if(currentQuestion < previousQuestion) {
+        currentQuestion++;
+        displayQuestion();
+    var answerButton = document.getElementsByClassName("answer-button");
+    for (var i = 0; i < answerButton.length; i++) {
+        answerButton[i].addEventListener("click", nextQuestion)
+    };
+    console.log(currentQuestion);
+    } else {
+        return;
+    }
+
 }
