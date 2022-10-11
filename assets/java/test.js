9d0a7d55a26f6c855eca2ae0fdbbd0130c18bfe2
Alex Clayton <alexander.clayton.email@gmail.com>
2022-10-11 03:22:39 -0500

    added timer, score calculator

--- assets/java/test.js
+++ assets/java/test.js
@@ -1,3 +1,5 @@
+var highScoresLink = document.getElementById("high-scores-link");
+var timer = document.getElementById("timer");
 var introCard = document.getElementById("intro-card");
 var startButton = document.getElementById("start-button");
 var questionCard = document.getElementById("question-card")
@@ -8,6 +10,7 @@ var optionB = document.getElementById("option-b");
 var optionC = document.getElementById("option-c");
 var optionD = document.getElementById("option-d");
 var check = document.getElementById("check-answer");
+var finalScore = document.getElementById("final-score");
 var submitCard = document.getElementById("submit-card");
 var submitScore = document.getElementById("submit-score-button");
 var highScoreCard = document.getElementById("high-score-card");
@@ -61,15 +64,37 @@ var score = 0;
 var currentQuestion = 0;
 var questionCount = questions.length -1;
 
+highScoresLink.addEventListener("click", viewScores);
+
 startButton.addEventListener("click", startQuiz);
 
-submitScore.addEventListener("click", viewScores)
+submitScore.addEventListener("click", viewScores);
+
+returnToHomeButton.addEventListener("click", returnHome)
 
 function startQuiz() {
    introCard.style.display = "none";
    questionCard.style.display = "block";
+   timer.style.display = "block";
+   countDown();
    displayQuestion();
+   
 };
+
+//issues clearing interval??
+
+function countDown() {
+    var secondsLeft = 60;
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.innerHTML = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            enterInitials();
+        }
+    }, 1000)
+}
 
 function displayQuestion() {
     var thisQuestion = questions[currentQuestion];
@@ -85,9 +110,8 @@ function checkAnswer(event) {
     var thisQuestion = questions[currentQuestion];
     if (event.target.id == thisQuestion.answer) {
         score++;
-    } else {
     };
-    nextQuestion();
+    nextQuestion()
 };
 
 function nextQuestion() {
@@ -100,8 +124,14 @@ function nextQuestion() {
 };
 
 function enterInitials() {
-    questionCard.style.display = "none"
-    submitCard.style.display = "block"
+    timer.style.display = "none";
+    introCard.style.display = "none";
+    questionCard.style.display = "none";
+    submitCard.style.display = "block";
+    highScoreCard.style.display = "none";
+    var scorePercent = ((score / 5) * 100)
+    finalScore.innerHTML = "You finished with a score of " + scorePercent + "%!";
+    
 };
 
 function viewScores() {
@@ -109,8 +139,17 @@ function viewScores() {
     questionCard.style.display = "none";
     submitCard.style.display = "none";
     highScoreCard.style.display = "block";
-    console.log("hi")
+    
 }
+
+function returnHome () {
+    timer.style.display = "none";
+    introCard.style.display = "block";
+    questionCard.style.display = "none";
+    submitCard.style.display = "none";
+    highScoreCard.style.display = "none";
+}
+
 
 /*function nextQuestion() {
     if (userAnswer[i] === answerKey[i]) {
